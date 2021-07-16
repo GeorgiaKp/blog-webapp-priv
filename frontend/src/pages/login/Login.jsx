f1f2a8d94d733f844a9a223b08812a1142b3d45c
GeorgiaKp <georgiakpd@hotmail.com>
2021-07-16 17:34:00 +0300

    .gitignore.txt

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -2,17 +2,30 @@ import { Link } from 'react-router-dom'
 import "./login.css";
 
 export default function Login() {
+
+  const userRef = useRef()
+  const passwordRef = useRef()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
     <div className="login">
       <span className="loginTitle">Login</span>
-      <form className="loginForm">
+      <form className="loginForm" onSubmit={handleSubmit}>
         <label>Email</label>
         <input className="loginInput" type="email" placeholder="Enter your email..." />
         <label>Password</label>
-        <input className="loginInput" type="password" placeholder="Enter your password..." />
+        <input 
+          className="loginInput" 
+          type="password" 
+          placeholder="Enter your password."
+          ref={passwordRef}
+        />
         <button className="loginButton">Login</button>
       </form>
-        <button className="loginRegisterButton">
+        <button className="loginRegisterButton" type="submit">
           <Link className="link" to="/register">Register</Link>
         </button>
     </div>
