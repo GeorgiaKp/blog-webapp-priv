8cd65f54f36fd4bb6629828a40f75edee40ddfb1
GeorgiaKp <georgiakpd@hotmail.com>
2021-07-21 13:08:28 +0300

    added task lint

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -16,7 +16,7 @@ router.post("/register", async (req, res) => {
     const user = await newUser.save();
     res.status(200).json(user);
   } catch (err) {
-    res.status(500).json(err);
+    console.error(500);
   }
 });
 
