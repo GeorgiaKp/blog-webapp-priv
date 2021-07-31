ead2de3021ca95aec07fe959b03b40f737128d56
GeorgiaKp <georgiakpd@hotmail.com>
2021-07-31 08:59:35 +0300

    server

--- api/index.js
+++ api/index.js
@@ -1,56 +1,11 @@
-const express = require('express');
-
-const app = express();
-const path = require('path');
 const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-const multer = require('multer');
-const authRoute = require('./routes/auth');
-const userRoute = require('./routes/users');
-const postRoute = require('./routes/posts');
-const categoryRoute = require('./routes/categories');
+const AppServer = require('./appServer')
 
-async function main() {
-  dotenv.config();
-  app.use(express.json());
-  app.use('/images', express.static(path.join(__dirname, '/images')));
+dotenv.config();
 
-  try {
-    await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: true,
-    });
-  } catch (err) {
-    console.log(err);
-    return;
-  }
-  console.log('Connected to MongoDB');
+const PORT = process.env.port || 5000;
+const appServer = new AppServer(PORT);
+appServer.run()
 
-  const storage = multer.diskStorage({
-    destination: (req, file, callback) => {
-      callback(null, 'images');
-    },
-    filename: (req, file, callback) => {
-      callback(null, req.body.name);
-    },
-  });
 
-  const upload = multer({ storage });
-  app.post('/api/upload', upload.single('file'), (req, res) => {
-    res.status(200).json('File has been uploaded');
-  });
 
-  const apiPrefix = '/api/';
-  app.use(`${apiPrefix}auth`, authRoute.router);
-  app.use(`${apiPrefix}users`, userRoute);
-  app.use(`${apiPrefix}posts`, postRoute);
-  app.use(`${apiPrefix}categories`, categoryRoute);
-
-  app.listen(13371, '127.0.0.1', () => {
-    console.log('Server up');
-  });
-}
-
-main();
