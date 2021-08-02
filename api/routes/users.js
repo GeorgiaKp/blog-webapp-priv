6fc4a3f25dc14880f28659e3076c543c4313ddc7
GeorgiaKp <georgiakpd@hotmail.com>
2021-08-02 13:23:33 +0300

    users.js updated

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,27 +5,36 @@ const bcrypt = require("bcrypt");
 
 async function UserUpdateController(req, res) {
   if (req.body.userId === req.params.id) {
-    if (req.body.password) {
-      const salt = await bcrypt.genSalt(10);
-      req.body.password = await bcrypt.hash(req.body.password, salt);
-    }
+
     let oldUser;
     try {
       oldUser = await User.findById(req.params.id);
     } catch (err) {
       return res.status(404).json("User not found!");
     }
+
+    if (req.body.password.length>=6) {
+      try {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      } catch (err) {
+        return res.status(500).json("There was an error!");
+      }
+    } else {
+      return res.status(403).json("You need a password with 6+ characters");
+    }
+    
     try {
       const updatedUser = await User.findByIdAndUpdate(
         req.params.id,
         {
           $set: req.body,
-        },
-        { new: true }
+        }
       );
       await Post.updateMany({ username: oldUser.username }, 
         { $set: { username: updatedUser.username } },
       );
+      // console.log(updatedUser)
       res.status(200).json(updatedUser);
     } catch (err) {
       res.status(500).json("There was an error");
@@ -50,6 +59,7 @@ async function UserDeleteController(req, res) {
     return res.status(401).json("You can delete only your account!");
   }
   try {
+    // console.log({ username: user.username })
     await Post.deleteMany({ username: user.username });
     await User.findByIdAndDelete(req.params.id);
     res.status(200).json("User has been deleted.");
@@ -64,8 +74,10 @@ router.delete("/:id", UserDeleteController)
 
 async function UserGetController(req, res)  {
   try {
-    const user = await User.findById(req.params.id);
-    const { password, ...others } = user._doc;
+    const user = await User.findById(req.params.id, {}, {lean:true});
+    // const user = await User.findById(req.params.id).lean().exec();
+    const { password, ...others } = user;
+    // const { password, ...others } = user.toJSON();
     res.status(200).json(others);
   } catch (err) {
     res.status(500).json("There was an error");
@@ -76,11 +88,10 @@ async function UserGetController(req, res)  {
 router.get("/:id", UserGetController)
 
 
-// module.exports = {
-//   UserGetController,
-//   UserUpdateController,
-//   UserDeleteController,
-//   router
-// };
+module.exports = {
+  UserGetController,
+  UserUpdateController,
+  UserDeleteController,
+  router
+};
 
-module.exports = router;
