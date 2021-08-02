ca37b6df8660ff6c7a024f6db8db1418673fde75
GeorgiaKp <georgiakpd@hotmail.com>
2021-08-02 09:44:16 +0300

    corrected tests

--- api/tests/deleteUser.test.js
+++ api/tests/deleteUser.test.js
@@ -4,6 +4,7 @@ const modelP = require('../models/Post');
 const httpMock = require('node-mocks-http');
 const mockUserList = require('./mockdata/users.json')
 
+
 modelU.findByIdAndDelete = jest.fn();
 modelU.findById = jest.fn();
 modelP.deleteMany = jest.fn();
@@ -21,43 +22,66 @@ describe("users controller: delete user by id",()=>{
         expect(typeof users.UserDeleteController).toBe('function')
     });
 
-    test("User.findByIdAndDelete throws exception when user not found", async () => {
-        req.params.id = mockUserList[0]._doc._id;
+    test("404 when user not found", async () => {
+        req.params.id = mockUserList[0]._id;
         modelU.findById.mockRejectedValue("fake exception from findbyid");
-        const res = {
-            send: jest.fn(s => s)
-        };
         await users.UserDeleteController(req, res);
         expect(modelU.findById).toHaveBeenCalledWith(req.params.id);
-        expect(res.send.mock.calls.length).toEqual(1);
-        expect(res.send.mock.calls[0][0]).toEqual('User not found!');
+        expect(res.statusCode).toEqual(404);
+        expect(res._getJSONData()).toStrictEqual('User not found!');
     })
 
-    test.only("delete a valid user", async () => {
-        req.params.id = mockUserList[0]._doc._id;
-        req.body.userId = mockUserList[0]._doc._id;
-        modelU.findById.mockReturnValue(req.params.id);
-        modelP.deleteMany.mockReturnValue({ username: mockUserList[0]._doc.username })
+    test("401 when to be deleted user is not yours", async ()=> {
+        req.params.id = mockUserList[0]._id;
+        req.body.userId = mockUserList[1]._id;
+        modelU.findById.mockReturnValue(req.params.id)
+        await users.UserDeleteController(req, res);
+        expect(req.params.id).not.toEqual(req.body.userId)
+        expect(res.statusCode).toEqual(401);
+        expect(res._getJSONData()).toStrictEqual("You can delete only your account!")
+        
+    });
+
+    test("500 when Post.deleteMany throws error", async ()=> {
+        req.params.id = mockUserList[0]._id;
+        req.body.userId = mockUserList[0]._id;
+        modelU.findById.mockReturnValue(req.params.id)
+        modelP.deleteMany.mockRejectedValue("fake exception from deleteMany")
         modelU.findByIdAndDelete.mockReturnValue(req.params.id);
         await users.UserDeleteController(req, res);
-        expect(modelP.deleteMany).toBeCalledWith({ username: mockUserList[0]._doc.username });
+        expect(req.params.id).toEqual(req.body.userId)
+        expect(res.statusCode).toEqual(500);
+        expect(res._getJSONData()).toStrictEqual("There was an error")
+        
+    });
+
+    test("500 when User.findByIdAndDelete throws error", async ()=> {
+        req.params.id = mockUserList[0]._id;
+        req.body.userId = mockUserList[0]._id;
+        modelU.findById.mockReturnValue((req.params.id))
+        modelP.deleteMany.mockReturnValue({ username: mockUserList[0].username })
+        modelU.findByIdAndDelete.mockRejectedValue("fake exception from deleteMany");
+        await users.UserDeleteController(req, res);
+        expect(req.params.id).toEqual(req.body.userId)
+        expect(res.statusCode).toEqual(500);
+        expect(res._getJSONData()).toStrictEqual("There was an error")
+        
+    });
+
+    test("delete a valid user", async () => {
+        req.params.id = mockUserList[0]._id;
+        req.body.userId = mockUserList[0]._id;
+        modelU.findById.mockReturnValue(mockUserList[0]);
+        modelP.deleteMany.mockReturnValue({ username: mockUserList[0].username })
+        modelU.findByIdAndDelete.mockReturnValue(req.params.id);
+        await users.UserDeleteController(req, res);
+        expect(req.params.id).toEqual(req.body.userId)
+        expect(modelP.deleteMany).toBeCalledWith({ username : mockUserList[0].username });
         expect(modelU.findByIdAndDelete).toBeCalledWith(req.params.id);
         expect(res.statusCode).toEqual(200);
         expect(res._getJSONData()).toStrictEqual("User has been deleted.")
     });
 
-    test("error msg when to be deleted user is not yours", async ()=> {
-        req.params.id = mockUserList[0]._doc._id;
-        req.body.userId = mockUserList[1]._doc._id;
-        modelU.findById.mockReturnValue(req.params.id)
-        const res = {
-            send: jest.fn(s => s)
-        };
-        await users.UserDeleteController(req, res);
-        expect(req.params.id).not.toEqual(req.body.userId)
-        expect(res.send.mock.calls.length).toEqual(1);
-        expect(res.send.mock.calls[0][0]).toEqual("You can delete only your account!");
-        
-    });
+    
 
 });
--- api/tests/getUser.test.js
+++ api/tests/getUser.test.js
@@ -21,26 +21,48 @@ describe("users controller: get user by id",()=>{
     });
 
     test("return a user by id", async () => {
-        req.params.id = mockUserList[0]._doc._id;
+        req.params.id = mockUserList[0]._id;
         model.findById.mockReturnValue(mockUserList[0]);
+        const { password, ...others } = mockUserList[0];
         await users.UserGetController(req, res);
-        expect(model.findById).toHaveBeenCalledWith(req.params.id);
-        expect(res.statusCode).toBe(200);
-        const { password, ...others } = mockUserList[0]._doc;
+        expect(model.findById).toHaveBeenCalledWith(req.params.id, {}, {lean:true});
+        expect(res.statusCode).toEqual(200);
         expect(res._getJSONData()).toStrictEqual(others);
     })
 
-    test("User.findById throws exception when needed", async()=>{
+    test("500 when User.findById throws error", async()=>{
         req.params.id = mockUserList[0]._id;
         model.findById.mockRejectedValue("fake exception from findbyid");
-        const res = {
-            send: jest.fn(s => s)
-        };
         await users.UserGetController(req, res);
-        expect(model.findById).toHaveBeenCalledWith(req.params.id);
-        expect(res.send.mock.calls.length).toEqual(1);
-        expect(res.send.mock.calls[0][0]).toEqual('There was an error');
+        expect(model.findById).toHaveBeenCalledWith(req.params.id, {}, {lean:true});
+        expect(res.statusCode).toEqual(500);
+        expect(res._getJSONData()).toStrictEqual("There was an error")
     })
+
+//     test('Mock toJSON with Spy and Return Value', () => {
+//     const message = "test string ()@#$%^";
+//     const spy = jest.spyOn(global, 'toJSON').mockReturnValueOnce('Mock Value');
+//     expect(yourFunction(message)).toBe('Mock Value');
+// });
+
+//     test('Mock Encode URI component', () => {
+//     // Store original implementation
+//     const originalEncode = toJSON;
+
+//     const message = "test string ()@#$%^";
+//     encodeURIComponent = jest.fn(() => 'Mock Value');
+//     expect(yourFunction(message)).toBe('Mock Value');
+
+//     // Restore original implementation
+//     encodeURIComponent = originalEncode;
+// });
+
+//     test('stub .toBeCalled()', () => {
+//       model.stub = jest.fn();
+//       console.log(model.stub)
+//       model.stub();
+//       expect(model.stub).toBeCalled();
+//     });
 
 })
 
